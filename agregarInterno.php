353381c576af843004311b54bafd6846d63e7843
walysion <89873423+walysion@users.noreply.github.com>
2023-06-19 18:13:42 -0400

    version2.3

--- agregarInterno.php
+++ agregarInterno.php
@@ -145,25 +145,7 @@
          
         </div>
          <input class="btn btn-primary" style="display: flex; margin: 0px auto; margin-top:70px" type="submit" value="Enviar" data-toggle="modal" data-target="#exampleModalCenter">
-         <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>      
+               
       </form>
      </div>
 
